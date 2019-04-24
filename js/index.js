2d2544e93f6a2ff0d9a4a7c27bb7ba3177d16502
Matheus Souza <matheu.sk.9@hotmail.com>
2019-04-24 20:17:50 -0300

    Add css animation on caroussel transition

--- js/index.js
+++ js/index.js
@@ -8,16 +8,41 @@ const isShown = element => {
   return element.classList.contains('show') ? true : false;
 };
 
-// Specific Functionality
+// Specific Functionality - Arrows
 rightArrow.addEventListener('click', () => {
   for (let i = 0; i < comments.length; i++) {
     if (isShown(comments[i])) {
-      comments[i].classList.remove('show');
       if (i === comments.length - 1) {
-        comments[0].classList.add('show');
+        comments[i].classList.add('animated');
+        comments[i].classList.add('bounceOutLeft');
+        setTimeout(() => {
+          comments[i].classList.remove('show');
+          comments[i].classList.remove('animated');
+          comments[i].classList.remove('bounceOutLeft');
+          comments[0].classList.add('show');
+          comments[0].classList.add('animated');
+          comments[0].classList.add('bounceInRight');
+        }, 1000);
+        setTimeout(() => {
+          comments[0].classList.remove('animated');
+          comments[0].classList.remove('bounceInRight');
+        }, 1500);
         return;
       } else {
-        comments[i + 1].classList.add('show');
+        comments[i].classList.add('animated');
+        comments[i].classList.add('bounceOutLeft');
+        setTimeout(() => {
+          comments[i].classList.remove('show');
+          comments[i].classList.remove('animated');
+          comments[i].classList.remove('bounceOutLeft');
+          comments[i + 1].classList.add('show');
+          comments[i + 1].classList.add('animated');
+          comments[i + 1].classList.add('bounceInRight');
+        }, 1000);
+        setTimeout(() => {
+          comments[i + 1].classList.remove('animated');
+          comments[i + 1].classList.remove('bounceInRight');
+        }, 1500);
         return;
       }
     }
@@ -27,12 +52,37 @@ rightArrow.addEventListener('click', () => {
 leftArrow.addEventListener('click', () => {
   for (let i = comments.length - 1; i >= 0; i--) {
     if (isShown(comments[i])) {
-      comments[i].classList.remove('show');
       if (i === 0) {
-        comments[comments.length - 1].classList.add('show');
+        comments[i].classList.add('animated');
+        comments[i].classList.add('bounceOutRight');
+        setTimeout(() => {
+          comments[i].classList.remove('show');
+          comments[i].classList.remove('animated');
+          comments[i].classList.remove('bounceOutRight');
+          comments[comments.length - 1].classList.add('show');
+          comments[comments.length - 1].classList.add('animated');
+          comments[comments.length - 1].classList.add('bounceInLeft');
+        }, 1000);
+        setTimeout(() => {
+          comments[comments.length - 1].classList.remove('animated');
+          comments[comments.length - 1].classList.remove('bounceInLeft');
+        }, 1500);
         return;
       } else {
-        comments[i - 1].classList.add('show');
+        comments[i].classList.add('animated');
+        comments[i].classList.add('bounceOutRight');
+        setTimeout(() => {
+          comments[i].classList.remove('show');
+          comments[i].classList.remove('animated');
+          comments[i].classList.remove('bounceOutRight');
+          comments[i - 1].classList.add('show');
+          comments[i - 1].classList.add('animated');
+          comments[i - 1].classList.add('bounceInLeft');
+        }, 1000);
+        setTimeout(() => {
+          comments[i - 1].classList.remove('animated');
+          comments[i - 1].classList.remove('bounceInLeft');
+        }, 1500);
         return;
       }
     }
