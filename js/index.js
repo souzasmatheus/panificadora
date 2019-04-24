c68cbf9a92fd3636c0a350c7146eb1280d65163b
Matheus Souza <matheu.sk.9@hotmail.com>
2019-04-24 20:49:12 -0300

    Implement automatic caroussel transition

--- js/index.js
+++ js/index.js
@@ -3,13 +3,63 @@ const leftArrow = document.querySelector('#arrow-left');
 const rightArrow = document.querySelector('#arrow-right');
 const comments = document.querySelectorAll('.comment-container');
 
+let interval;
+
 // Helper functions
 const isShown = element => {
   return element.classList.contains('show') ? true : false;
 };
 
+// Specific Functionality - Automatic Change
+function automaticChange() {
+  interval = setInterval(() => {
+    for (let i = 0; i < comments.length; i++) {
+      if (isShown(comments[i])) {
+        if (i === comments.length - 1) {
+          comments[i].classList.add('animated');
+          comments[i].classList.add('bounceOutLeft');
+          setTimeout(() => {
+            comments[i].classList.remove('show');
+            comments[i].classList.remove('animated');
+            comments[i].classList.remove('bounceOutLeft');
+            comments[0].classList.add('show');
+            comments[0].classList.add('animated');
+            comments[0].classList.add('bounceInRight');
+          }, 1000);
+          setTimeout(() => {
+            comments[0].classList.remove('animated');
+            comments[0].classList.remove('bounceInRight');
+          }, 1500);
+          return;
+        } else {
+          comments[i].classList.add('animated');
+          comments[i].classList.add('bounceOutLeft');
+          setTimeout(() => {
+            comments[i].classList.remove('show');
+            comments[i].classList.remove('animated');
+            comments[i].classList.remove('bounceOutLeft');
+            comments[i + 1].classList.add('show');
+            comments[i + 1].classList.add('animated');
+            comments[i + 1].classList.add('bounceInRight');
+          }, 1000);
+          setTimeout(() => {
+            comments[i + 1].classList.remove('animated');
+            comments[i + 1].classList.remove('bounceInRight');
+          }, 1500);
+          return;
+        }
+      }
+    }
+  }, 4000);
+}
+
+function clearAutomaticChange() {
+  clearInterval(interval);
+}
+
 // Specific Functionality - Arrows
 rightArrow.addEventListener('click', () => {
+  clearAutomaticChange();
   for (let i = 0; i < comments.length; i++) {
     if (isShown(comments[i])) {
       if (i === comments.length - 1) {
@@ -27,6 +77,7 @@ rightArrow.addEventListener('click', () => {
           comments[0].classList.remove('animated');
           comments[0].classList.remove('bounceInRight');
         }, 1500);
+        automaticChange();
         return;
       } else {
         comments[i].classList.add('animated');
@@ -43,6 +94,7 @@ rightArrow.addEventListener('click', () => {
           comments[i + 1].classList.remove('animated');
           comments[i + 1].classList.remove('bounceInRight');
         }, 1500);
+        automaticChange();
         return;
       }
     }
@@ -50,6 +102,7 @@ rightArrow.addEventListener('click', () => {
 });
 
 leftArrow.addEventListener('click', () => {
+  clearAutomaticChange();
   for (let i = comments.length - 1; i >= 0; i--) {
     if (isShown(comments[i])) {
       if (i === 0) {
@@ -67,6 +120,7 @@ leftArrow.addEventListener('click', () => {
           comments[comments.length - 1].classList.remove('animated');
           comments[comments.length - 1].classList.remove('bounceInLeft');
         }, 1500);
+        automaticChange();
         return;
       } else {
         comments[i].classList.add('animated');
@@ -83,8 +137,12 @@ leftArrow.addEventListener('click', () => {
           comments[i - 1].classList.remove('animated');
           comments[i - 1].classList.remove('bounceInLeft');
         }, 1500);
+        automaticChange();
         return;
       }
     }
   }
 });
+
+// Trigger Functions
+automaticChange();
