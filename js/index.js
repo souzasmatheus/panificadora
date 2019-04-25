aeb83740d3c79a197ad5cc92d71592bcb6c7153b
Matheus Souza <matheu.sk.9@hotmail.com>
2019-04-25 04:19:41 -0300

    Make caroussel work on devices bigger than 880 pixels

--- js/index.js
+++ js/index.js
@@ -10,139 +10,151 @@ const isShown = element => {
   return element.classList.contains('show') ? true : false;
 };
 
-// Specific Functionality - Automatic Change
-function automaticChange() {
-  interval = setInterval(() => {
-    for (let i = 0; i < comments.length; i++) {
-      if (isShown(comments[i])) {
-        if (i === comments.length - 1) {
-          comments[i].classList.add('animated');
-          comments[i].classList.add('bounceOutLeft');
-          setTimeout(() => {
-            comments[i].classList.remove('show');
-            comments[i].classList.remove('animated');
-            comments[i].classList.remove('bounceOutLeft');
-            comments[0].classList.add('show');
-            comments[0].classList.add('animated');
-            comments[0].classList.add('bounceInRight');
-          }, 1000);
-          setTimeout(() => {
-            comments[0].classList.remove('animated');
-            comments[0].classList.remove('bounceInRight');
-          }, 1500);
-          return;
-        } else {
-          comments[i].classList.add('animated');
-          comments[i].classList.add('bounceOutLeft');
-          setTimeout(() => {
-            comments[i].classList.remove('show');
-            comments[i].classList.remove('animated');
-            comments[i].classList.remove('bounceOutLeft');
-            comments[i + 1].classList.add('show');
-            comments[i + 1].classList.add('animated');
-            comments[i + 1].classList.add('bounceInRight');
-          }, 1000);
-          setTimeout(() => {
-            comments[i + 1].classList.remove('animated');
-            comments[i + 1].classList.remove('bounceInRight');
-          }, 1500);
-          return;
+// Check Window Size
+function checkSize() {
+  if (window.innerWidth >= 880) {
+    // Specific Functionality - Automatic Change
+    function automaticChange() {
+      interval = setInterval(() => {
+        for (let i = 0; i < comments.length; i++) {
+          if (isShown(comments[i])) {
+            if (i === comments.length - 1) {
+              comments[i].classList.add('animated');
+              comments[i].classList.add('bounceOutLeft');
+              setTimeout(() => {
+                comments[i].classList.remove('show');
+                comments[i].classList.remove('animated');
+                comments[i].classList.remove('bounceOutLeft');
+                comments[0].classList.add('show');
+                comments[0].classList.add('animated');
+                comments[0].classList.add('bounceInRight');
+              }, 1000);
+              setTimeout(() => {
+                comments[0].classList.remove('animated');
+                comments[0].classList.remove('bounceInRight');
+              }, 1500);
+              return;
+            } else {
+              comments[i].classList.add('animated');
+              comments[i].classList.add('bounceOutLeft');
+              setTimeout(() => {
+                comments[i].classList.remove('show');
+                comments[i].classList.remove('animated');
+                comments[i].classList.remove('bounceOutLeft');
+                comments[i + 1].classList.add('show');
+                comments[i + 1].classList.add('animated');
+                comments[i + 1].classList.add('bounceInRight');
+              }, 1000);
+              setTimeout(() => {
+                comments[i + 1].classList.remove('animated');
+                comments[i + 1].classList.remove('bounceInRight');
+              }, 1500);
+              return;
+            }
+          }
+        }
+      }, 4000);
+    }
+
+    function clearAutomaticChange() {
+      clearInterval(interval);
+    }
+
+    // Specific Functionality - Arrows
+    rightArrow.addEventListener('click', () => {
+      clearAutomaticChange();
+      for (let i = 0; i < comments.length; i++) {
+        if (isShown(comments[i])) {
+          if (i === comments.length - 1) {
+            comments[i].classList.add('animated');
+            comments[i].classList.add('bounceOutLeft');
+            setTimeout(() => {
+              comments[i].classList.remove('show');
+              comments[i].classList.remove('animated');
+              comments[i].classList.remove('bounceOutLeft');
+              comments[0].classList.add('show');
+              comments[0].classList.add('animated');
+              comments[0].classList.add('bounceInRight');
+            }, 1000);
+            setTimeout(() => {
+              comments[0].classList.remove('animated');
+              comments[0].classList.remove('bounceInRight');
+            }, 1500);
+            automaticChange();
+            return;
+          } else {
+            comments[i].classList.add('animated');
+            comments[i].classList.add('bounceOutLeft');
+            setTimeout(() => {
+              comments[i].classList.remove('show');
+              comments[i].classList.remove('animated');
+              comments[i].classList.remove('bounceOutLeft');
+              comments[i + 1].classList.add('show');
+              comments[i + 1].classList.add('animated');
+              comments[i + 1].classList.add('bounceInRight');
+            }, 1000);
+            setTimeout(() => {
+              comments[i + 1].classList.remove('animated');
+              comments[i + 1].classList.remove('bounceInRight');
+            }, 1500);
+            automaticChange();
+            return;
+          }
         }
       }
-    }
-  }, 4000);
+    });
+
+    leftArrow.addEventListener('click', () => {
+      clearAutomaticChange();
+      for (let i = comments.length - 1; i >= 0; i--) {
+        if (isShown(comments[i])) {
+          if (i === 0) {
+            comments[i].classList.add('animated');
+            comments[i].classList.add('bounceOutRight');
+            setTimeout(() => {
+              comments[i].classList.remove('show');
+              comments[i].classList.remove('animated');
+              comments[i].classList.remove('bounceOutRight');
+              comments[comments.length - 1].classList.add('show');
+              comments[comments.length - 1].classList.add('animated');
+              comments[comments.length - 1].classList.add('bounceInLeft');
+            }, 1000);
+            setTimeout(() => {
+              comments[comments.length - 1].classList.remove('animated');
+              comments[comments.length - 1].classList.remove('bounceInLeft');
+            }, 1500);
+            automaticChange();
+            return;
+          } else {
+            comments[i].classList.add('animated');
+            comments[i].classList.add('bounceOutRight');
+            setTimeout(() => {
+              comments[i].classList.remove('show');
+              comments[i].classList.remove('animated');
+              comments[i].classList.remove('bounceOutRight');
+              comments[i - 1].classList.add('show');
+              comments[i - 1].classList.add('animated');
+              comments[i - 1].classList.add('bounceInLeft');
+            }, 1000);
+            setTimeout(() => {
+              comments[i - 1].classList.remove('animated');
+              comments[i - 1].classList.remove('bounceInLeft');
+            }, 1500);
+            automaticChange();
+            return;
+          }
+        }
+      }
+    });
+
+    // Trigger Functions
+    automaticChange();
+  }
 }
 
-function clearAutomaticChange() {
+checkSize();
+
+window.addEventListener('resize', () => {
   clearInterval(interval);
-}
-
-// Specific Functionality - Arrows
-rightArrow.addEventListener('click', () => {
-  clearAutomaticChange();
-  for (let i = 0; i < comments.length; i++) {
-    if (isShown(comments[i])) {
-      if (i === comments.length - 1) {
-        comments[i].classList.add('animated');
-        comments[i].classList.add('bounceOutLeft');
-        setTimeout(() => {
-          comments[i].classList.remove('show');
-          comments[i].classList.remove('animated');
-          comments[i].classList.remove('bounceOutLeft');
-          comments[0].classList.add('show');
-          comments[0].classList.add('animated');
-          comments[0].classList.add('bounceInRight');
-        }, 1000);
-        setTimeout(() => {
-          comments[0].classList.remove('animated');
-          comments[0].classList.remove('bounceInRight');
-        }, 1500);
-        automaticChange();
-        return;
-      } else {
-        comments[i].classList.add('animated');
-        comments[i].classList.add('bounceOutLeft');
-        setTimeout(() => {
-          comments[i].classList.remove('show');
-          comments[i].classList.remove('animated');
-          comments[i].classList.remove('bounceOutLeft');
-          comments[i + 1].classList.add('show');
-          comments[i + 1].classList.add('animated');
-          comments[i + 1].classList.add('bounceInRight');
-        }, 1000);
-        setTimeout(() => {
-          comments[i + 1].classList.remove('animated');
-          comments[i + 1].classList.remove('bounceInRight');
-        }, 1500);
-        automaticChange();
-        return;
-      }
-    }
-  }
+  checkSize();
 });
-
-leftArrow.addEventListener('click', () => {
-  clearAutomaticChange();
-  for (let i = comments.length - 1; i >= 0; i--) {
-    if (isShown(comments[i])) {
-      if (i === 0) {
-        comments[i].classList.add('animated');
-        comments[i].classList.add('bounceOutRight');
-        setTimeout(() => {
-          comments[i].classList.remove('show');
-          comments[i].classList.remove('animated');
-          comments[i].classList.remove('bounceOutRight');
-          comments[comments.length - 1].classList.add('show');
-          comments[comments.length - 1].classList.add('animated');
-          comments[comments.length - 1].classList.add('bounceInLeft');
-        }, 1000);
-        setTimeout(() => {
-          comments[comments.length - 1].classList.remove('animated');
-          comments[comments.length - 1].classList.remove('bounceInLeft');
-        }, 1500);
-        automaticChange();
-        return;
-      } else {
-        comments[i].classList.add('animated');
-        comments[i].classList.add('bounceOutRight');
-        setTimeout(() => {
-          comments[i].classList.remove('show');
-          comments[i].classList.remove('animated');
-          comments[i].classList.remove('bounceOutRight');
-          comments[i - 1].classList.add('show');
-          comments[i - 1].classList.add('animated');
-          comments[i - 1].classList.add('bounceInLeft');
-        }, 1000);
-        setTimeout(() => {
-          comments[i - 1].classList.remove('animated');
-          comments[i - 1].classList.remove('bounceInLeft');
-        }, 1500);
-        automaticChange();
-        return;
-      }
-    }
-  }
-});
-
-// Trigger Functions
-automaticChange();
